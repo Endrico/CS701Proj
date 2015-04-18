fc22b6a55ee448eee871560d0b75594b21ab2ce3
56kuser <diwakarsomu@gmail.com>
2015-04-18 14:28:05 +1200

    ring counter test code
    assemblr ring counter

---
+++ mmil111 - assign1/ringcounter.asm
@@ -0,0 +1,14 @@
+start NOOP ;starting the program
+		LDR R1 #1
+		LDR R4 #16 ;16 bits before looping
+count	SUBV R4 R4 #1
+		PRESENT R4 start ;if R4=0 go to start
+		ADD R1 R1 R1 ;double the number
+		LSIP R0
+		OR R0 R0 R1 ;or it with LSIP
+		SSOP R0
+		LDR R3 #65535 ;max register size, 16 bits
+moretime SUBV R3 R3 #1
+		PRESENT R3 count ;if R3=0 go to time
+		JMP moretime
+ENDPROG
