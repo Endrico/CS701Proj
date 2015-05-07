9712e90efec6da210ea7fe5f90de1e2448433694
56kuser <diwakarsomu@gmail.com>
2015-05-08 11:40:36 +1200

    added tb

---
+++ PhaseII/test_bench.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity test_bench is 
+end test_bench;
+
+architecture behaviour of test_bench is 
+	component processor
+	
+	port(
+			-- place the stuff for the processor here all processor ports	
+			
+		 );
+	end component;
+	
